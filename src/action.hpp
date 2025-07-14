dac774c91a8b1cedc15969df89e7a6992fcfdc9f
Derek Gustafson <degustaf@gmail.com>
2025-07-14 08:40:50 -0400

    Add virtual destructors to actions

--- src/action.hpp
+++ src/action.hpp
@@ -6,9 +6,11 @@
 
 struct Action {
   virtual SDL_AppResult perform(int &x, int &y) const = 0;
+  virtual ~Action() = default;
 };
 
 struct ExitAction : Action {
+  virtual ~ExitAction() override = default;
   virtual SDL_AppResult perform([[maybe_unused]] int &x,
                                 [[maybe_unused]] int &y) const override {
     return SDL_APP_SUCCESS;
@@ -19,6 +21,7 @@ struct MoveAction : Action {
   std::array<int, 2> dxy;
 
   MoveAction(int x, int y) : dxy({x, y}){};
+  virtual ~MoveAction() override = default;
 
   virtual SDL_AppResult perform(int &x, int &y) const override {
     x += dxy[0];
