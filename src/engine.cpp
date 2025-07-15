d3e4e43d85dace1bd1c8ca3607f193ab30a1ea83
Derek Gustafson <degustaf@gmail.com>
2025-07-14 21:41:04 -0400

    Fix renderable background bug

--- src/engine.cpp
+++ src/engine.cpp
@@ -11,7 +11,11 @@ void Engine::render(flecs::world ecs) const {
   ecs.get_mut<GameMap>().render(console);
 
   auto q = ecs.query<const Position, const Renderable>();
-  q.each([&](auto p, auto r) { console.at(p) = {r.ch, r.color}; });
+  q.each([&](auto p, auto r) {
+    auto &tile = console.at(p);
+    tile.ch = r.ch;
+    tile.fg = r.color;
+  });
 
   ecs.get_mut<tcod::Context>().present(console);
   console.clear();
