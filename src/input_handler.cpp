298934b243eeac7bcb33f679691df22e12be861a
Derek Gustafson <degustaf@gmail.com>
2026-01-14 21:42:19 -0500

    Make bump action when clicked next to player

--- src/input_handler.cpp
+++ src/input_handler.cpp
@@ -92,8 +92,7 @@ std::unique_ptr<Action> EventHandler::dispatch(SDL_Event *event,
     return (this->*keyDown)(&event->key, ecs);
 
   case SDL_EVENT_MOUSE_MOTION:
-    mouse_loc[0] = (int)event->motion.x;
-    mouse_loc[1] = (int)event->motion.y;
+    mouse_loc = {(int)event->motion.x, (int)event->motion.y};
     return nullptr;
 
   case SDL_EVENT_MOUSE_BUTTON_DOWN:
@@ -564,11 +563,23 @@ std::unique_ptr<Action> EventHandler::EmptyClick(SDL_MouseButtonEvent *,
 }
 
 std::unique_ptr<Action>
-EventHandler::MainGameClick(SDL_MouseButtonEvent *button, flecs::world) {
+EventHandler::MainGameClick(SDL_MouseButtonEvent *button, flecs::world ecs) {
+  auto currentMap = ecs.lookup("currentMap").target<CurrentMap>();
+  auto &map = currentMap.get<GameMap>();
   if (commandBox[0] <= button->x && button->x < commandBox[0] + commandBox[2] &&
       commandBox[1] <= button->y && button->y < commandBox[1] + commandBox[3]) {
     commandsMenu();
     return nullptr;
+  } else if (map.inBounds((int)button->x, (int)button->y)) {
+    auto pos = ecs.lookup("player").get<Position>();
+    if (map.isExplored((int)button->x, (int)button->y)) {
+      if (pos.distanceSquared({(int)button->x, (int)button->y}) <= 2) {
+        return std::make_unique<BumpAction>((int)button->x - pos.x,
+                                            (int)button->y - pos.y, 1);
+      } else {
+        // TODO Add pathfinding action.
+      }
+    }
   }
   return nullptr;
 }
@@ -699,9 +710,9 @@ void EventHandler::AreaTargetOnRender(flecs::world ecs, tcod::Console &console,
 void EventHandler::MainMenuOnRender(flecs::world, tcod::Console &console,
                                     uint64_t) {
   static constexpr auto ImageWidth = 100;
-  static const auto background_image = TCODImage("assets/teeth.png");
-  assert(background_image.getSize()[0] == ImageWidth);
-  tcod::draw_quartergraphics(console, background_image);
+  // static const auto background_image = TCODImage("assets/teeth.png");
+  // assert(background_image.getSize()[0] == ImageWidth);
+  // tcod::draw_quartergraphics(console, background_image);
 
   const auto printY = (ImageWidth / 2 + console.get_width()) / 2;
   tcod::print(console, {printY, console.get_height() / 2 - 4},
