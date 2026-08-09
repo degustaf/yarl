b0e0ad72d571511bf3ac85d387244b8f75f2675a
Derek Gustafson <degustaf@gmail.com>
2026-08-09 08:42:44 -0400

    Remove dependency on TCOD

---
+++ src/random.cpp
@@ -0,0 +1,104 @@
+#include "random.hpp"
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+
+uint32_t Random::getU32(void) {
+  cur = (cur + 1) & (Q_SIZE - 1);
+  auto t = (uint64_t)(((uint64_t)187822) * Q[cur] + c);
+  c = (uint32_t)(t >> 32);
+  auto x = (uint32_t)(t + c);
+  if (x < c) {
+    x++;
+    c++;
+  }
+  if ((x + 1) == 0) {
+    c++;
+    x = 0;
+  }
+  Q[cur] = 0xfffffffe - x;
+  return Q[cur];
+}
+
+int Random::getInt(int min, int max) {
+  if (min == max)
+    return min;
+  assert(min < max);
+  auto delta = max - min + 1;
+  return (int)getU32() % delta + min;
+}
+
+float Random::getFloat(float min, float max) {
+  if (min == max)
+    return min;
+  assert(min < max);
+  auto delta = max - min;
+  return get<float>() * delta + min;
+}
+
+double Random::getDouble(double min, double max) {
+  if (min == max)
+    return min;
+  assert(min < max);
+  auto delta = max - min;
+  return get<double>() * delta + min;
+}
+
+static inline float cubic(float x) { return (3 - 2 * x) * x * x; }
+
+template <typename T> static inline T lerp(T a, T b, T x) {
+  return a + x * (b - a);
+}
+
+template <> float Noise<2>::get(const float (&pt)[2]) {
+  int n[2];
+  float r[2];
+  float w[2];
+
+  for (int i = 0; i < 2; i++) {
+    n[i] = (int)std::floor(pt[i]);
+    r[i] = pt[i] - (float)n[i];
+    w[i] = cubic(r[i]);
+  }
+
+  auto value = lerp(
+      lerp(lattice(n, r), lattice({n[0] + 1, n[1]}, {r[0] - 1, r[1]}), w[0]),
+      lerp(lattice({n[0], n[1] + 1}, {r[0], r[1] - 1}),
+           lattice({n[0] + 1, n[1] + 1}, {r[0] - 1, r[1] - 1}), w[0]),
+      w[1]);
+
+  return std::clamp(value, -1.0f, 1.0f);
+}
+
+template <> float Noise<3>::get(const float (&pt)[3]) {
+  int n[3];
+  float r[3];
+  float w[3];
+
+  for (int i = 0; i < 2; i++) {
+    n[i] = (int)std::floor(pt[i]);
+    r[i] = pt[i] - (float)n[i];
+    w[i] = cubic(r[i]);
+  }
+
+  auto value = lerp(
+      lerp(lerp(lattice(n, r),
+                lattice({n[0] + 1, n[1], n[2]}, {r[0] - 1, r[1], r[2]}), w[0]),
+           lerp(lattice({n[0], n[1] + 1, n[2]}, {r[0], r[1] - 1, r[2]}),
+                lattice({n[0] + 1, n[1] + 1, n[2]}, {r[0] - 1, r[1] - 1, r[2]}),
+                w[0]),
+           w[1]),
+
+      lerp(lerp(lattice({n[0], n[1], n[2] + 1}, {r[0], r[1], r[2] - 1}),
+                lattice({n[0] + 1, n[1], n[2] + 1}, {r[0] - 1, r[1], r[2] - 1}),
+                w[0]),
+           lerp(lattice({n[0], n[1] + 1, n[2] + 1}, {r[0], r[1] - 1, r[2] - 1}),
+                lattice({n[0] + 1, n[1] + 1, n[2] + 1},
+                        {r[0] - 1, r[1] - 1, r[2] - 1}),
+                w[0]),
+           w[1]),
+      w[2]);
+
+  return std::clamp(value, -1.0f, 1.0f);
+}
--- src/random.hpp
+++ src/random.hpp
@@ -1,25 +1,42 @@
 #pragma once
 
 #include <algorithm>
-
-#include <libtcod.hpp>
+#include <cmath>
+#include <cstdint>
+#include <ctime>
 
 struct Random {
-  Random(uint32_t seed) : rng(seed) {};
+  Random(uint32_t seed) {
+    uint32_t s = seed;
+    for (int i = 0; i < Q_SIZE; ++i)
+      Q[i] = s = (s * m) + b;
+    c = ((s * m) + b) % divisor;
+    cur = 0;
+  };
 
   static inline Random *getInstance() {
     static Random *instance = new Random();
     return instance;
   }
-  inline int getInt(int min, int max) { return rng.getInt(min, max); }
-  inline float getFloat(float min, float max) { return rng.getFloat(min, max); }
-  inline double getDouble(double min, double max) {
-    return rng.getDouble(min, max);
+  uint32_t getU32(void);
+  int getInt(int min, int max);
+  template <typename T> inline T get() {
+    return T(getU32()) * (T(1.0) / T(0xffffffff));
   }
+  float getFloat(float min, float max);
+  double getDouble(double min, double max);
 
 private:
-  Random() : rng() {};
-  TCODRandom rng;
+  static const auto constexpr Q_SIZE = 4096;
+  static const auto constexpr m = 1103515245;
+  static const auto constexpr b = 12345;
+  static const auto constexpr divisor = 809430660;
+
+  Random() : Random((uint32_t)time(0)) {};
+
+  uint32_t Q[Q_SIZE];
+  uint32_t c;
+  int cur;
 };
 
 template <typename _RandomAccessIterator, typename _RandomNumberGenerator>
@@ -36,9 +53,42 @@ void random_shuffle(_RandomAccessIterator __first, _RandomAccessIterator __last,
 
 template <int N> struct Noise {
 
-  Noise() : noise(N, N == 2 ? TCOD_NOISE_PERLIN : TCOD_NOISE_DEFAULT) {};
-  inline float get(const float (&pt)[N]) { return noise.get(pt); }
+  Noise() {
+    auto rng = Random::getInstance();
+    for (auto i = 0; i < 256; i++) {
+      map[i] = (uint8_t)i;
+      float magnitude = 0.0f;
+      for (auto j = 0; j < N; j++) {
+        buffer[i][j] = rng->getFloat(-0.5f, 0.5f);
+        magnitude += buffer[i][j] * buffer[i][j];
+      }
+
+      magnitude = 1.0f / std::sqrt(magnitude);
+      for (auto j = 0; j < N; j++) {
+        buffer[i][j] *= magnitude;
+      }
+    }
+
+    for (auto i = 255; i >= 0; i--) {
+      auto j = rng->getInt(0, 255);
+      std::swap(map[i], map[j]);
+    }
+  };
+  float get(const float (&pt)[N]);
 
 private:
-  TCODNoise noise;
+  float lattice(const int (&n)[N], const float (&f)[N]) const {
+    int nIndex = 0;
+    for (int i = 0; i < N; ++i) {
+      nIndex = map[(nIndex + n[i]) & 0xFF];
+    }
+    float value = 0;
+    for (int i = 0; i < N; ++i) {
+      value += buffer[nIndex][i] * f[i];
+    }
+    return value;
+  }
+
+  uint8_t map[256];
+  float buffer[256][N];
 };
