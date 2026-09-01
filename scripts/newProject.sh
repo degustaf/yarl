a3ed4e6b21b2f58e2c244206d03e7926eb7e589b
Derek Gustafson <degustaf@gmail.com>
2026-09-01 12:08:52 -0400

    Improve new project script

--- scripts/newProject.sh
+++ scripts/newProject.sh
@@ -7,4 +7,9 @@ git clone https://github.com/degustaf/yarl "$repo"
 cd "$repo"
 git remote rename origin upstream
 git remote add origin "https://github.com/${user}/${repo}.git"
-git push origin nonTCOD
+git push --set-upstream origin nonTCOD
+git push
+
+mkdir .githooks
+cp scripts/pre-push .githooks
+git config core.hooksPath .githooks
